39794e63f14313d3faaded71a1446183af3f1da2
skyrocket <skyrocket.kr@gmail.com>
2020-04-07 03:36:57 +0900

    1번그룹 작업 끝

--- dev/assets/js/function.js
+++ dev/assets/js/function.js
@@ -1,3 +1,18 @@
-$(document).ready(function(){
+(function($, window, document, undefined){
 
-})
+    $(function(){
+
+    });
+
+    $('.js-modal-close').on('click', function(e){
+        e.preventDefault();
+        $(this).parents('.modal').hide();
+    })
+
+
+
+})(jQuery, window, document);
+
+function showModal(el) {
+    document.querySelector(el).style.display = 'flex';
+}
